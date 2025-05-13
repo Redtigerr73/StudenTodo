27eb0ea903f876a9c8f6410c77c4d8c146bce9c1
riwan25 <riwan.claes@gmail.com>
2025-05-13 14:07:29 +0200

    add TodoItem to database

---
+++ StudenTodo/StudenTodo/Data/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿namespace StudenTodo.Data
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }  // Identifiant unique
+        public string Title { get; set; } = "";
+        public string Priority { get; set; } = "Normale";
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime? CompletedAt { get; set; } // Nullable pour les tâches non terminées
+        public string? Description { get; set; } // Nullable pour les tâches sans description
+
+    }
+}
